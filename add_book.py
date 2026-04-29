5352eb9925ddf56b247ae724edd6615e677ca13f
Piyush Dutt <piyushdutt102007@gmail.com>
2026-04-29 06:54:56 +0530

    Restore add_books function implementation

--- add_book.py
+++ add_book.py
@@ -3,17 +3,3 @@ def add_books():
     name=input("Enter the books name:")
     books.append(name)
     print("books added") 
-
-
-
-
-
-
-
-# def add_books():
-#     name = input("Enter the book name: ").strip().lower()
-#     if name in library_books:
-#         print("Book already exists")
-#     else:
-#         library_books[name] = {"status": "Available", "issued_on": None}
-#         print("Book added successfully")

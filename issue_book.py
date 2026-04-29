4ea0e7c04ac2b50dc2af64da20982ea173dc2838
Piyush Dutt <piyushdutt102007@gmail.com>
2026-04-29 06:55:24 +0530

    Refactor issue_books function implementation

--- issue_book.py
+++ issue_book.py
@@ -7,22 +7,3 @@ def issue_books():
         print("Book Issued")
     else:
         print("Book not available")
-
-
-
-
-
-
-
-
-# def issue_books():
-#     name = input("Enter the book name: ").strip().lower()
-#     if name in library_books:
-#         if library_books[name]["status"] == "Available":
-#             library_books[name]["status"] = "Issued"
-#             library_books[name]["issued_on"] = datetime.date.today()
-#             print("Book issued successfully")
-#         else:
-#             print("Book is already issued")
-#     else:
-#         print("Book not found")
